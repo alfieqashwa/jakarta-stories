b3103e9ea33c59c0d3358b04242853242ab2f9ed
Alfie Qashwa <alfieqashwa@gmail.com>
2024-05-02 17:22:50 +0700

    feat: config prettier-plugin-tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,35 @@
-import type { Metadata } from "next";
+import type { Metadata } from "next"
 // import { Lexend_Deca } from "next/font/google";
-import { Lexend_Deca as FontSans } from "next/font/google";
-import "./globals.css";
-import { cn } from "~/lib/utils";
+import { Lexend_Deca as FontSans } from "next/font/google"
+import "./globals.css"
+import { cn } from "~/lib/utils"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-});
+})
 
 export const metadata: Metadata = {
   title: "Jakarta Stories",
   description: "Jakarta Stories Description",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="scroll-smooth dark">
+    <html lang="en" suppressHydrationWarning className="dark scroll-smooth">
       <head />
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
+          fontSans.variable,
         )}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -79,4 +79,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+}
