585a7d1a9e8c021053977ee4d18deb4d6934fc4b
Alfie Qashwa <alfieqashwa@gmail.com>
2024-05-02 17:53:44 +0700

    feat: add images for sampling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next"
 // import { Lexend_Deca } from "next/font/google";
 import { Lexend_Deca as FontSans } from "next/font/google"
-import "./globals.css"
 import { cn } from "~/lib/utils"
+import "./globals.css"
 
 const fontSans = FontSans({
   subsets: ["latin"],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
 import Image from "next/image"
 import { Button } from "~/components/ui/button"
 
+const IMAGES = [
+  { src: "/images/23.png", alt: "23" },
+  { src: "/images/24.png", alt: "24" },
+  { src: "/images/25.png", alt: "25" },
+  { src: "/images/26.png", alt: "26" },
+  { src: "/images/27.png", alt: "27" },
+  { src: "/images/28.png", alt: "28" },
+  { src: "/images/29.png", alt: "29" },
+] as const
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-black">Jakarta Stories</h1>
-      <Button>Sample Button</Button>
+      <h1 className="kurt text-4xl font-black">Jakarta Stories</h1>
+      <Button variant={"secondary"} size={"lg"} className="mt-4">
+        Sample Button
+      </Button>
+      <div>
+        {IMAGES.map((img, i) => (
+          <Image
+            src={img.src}
+            alt={img.alt}
+            width={1200}
+            height={1200}
+            key={i}
+            className="py-4"
+          />
+        ))}
+      </div>
     </main>
   )
 }
