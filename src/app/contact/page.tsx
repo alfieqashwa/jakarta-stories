a4653ff31cf340e6b287bb974bd58b3dcf46f38b
Alfie Qashwa <alfieqashwa@gmail.com>
2024-05-05 12:30:41 +0700

    style:resize text on min-width 1024px (lg)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,13 +5,13 @@ export default function Contact() {
   return (
     <div className="flex min-h-[calc(100vh_-_4rem)] flex-col items-center p-24">
       <h2 className="text-5xl font-black capitalize lg:text-6xl">contact</h2>
-      <article className="flex flex-col items-center py-16 text-lg font-medium">
+      <article className="flex flex-col items-center py-16 text-lg font-medium lg:space-y-2 lg:text-xl">
         <section className="flex items-center">
-          <Mail className="mr-2 size-5" />
+          <Mail className="mr-2 size-5 lg:size-6" />
           <p>{EMAIL}</p>
         </section>
         <section className="flex items-center">
-          <Phone className="mr-2 size-5" />
+          <Phone className="mr-2 size-5 lg:size-6" />
           <p>(+62) 895-4117-61183</p>
         </section>
       </article>
