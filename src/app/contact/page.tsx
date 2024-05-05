1ed08e94a0239793575815ce7cf74472b662da8f
Alfie Qashwa <alfieqashwa@gmail.com>
2024-05-05 22:51:36 +0700

    add margin top to contact content

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,7 +10,7 @@ export default function Contact() {
   return (
     <div className="flex min-h-[calc(100vh_-_8rem)] flex-col items-center pt-12">
       <h2 className="text-5xl font-bold capitalize lg:text-6xl">contact</h2>
-      <article className="flex flex-col items-center py-16 text-lg font-medium lg:space-y-2 lg:text-xl">
+      <article className="mt-20 flex flex-col items-center py-16 text-lg font-medium lg:space-y-2 lg:text-xl">
         <section className="flex items-center">
           <Mail className="mr-2 size-5 lg:size-6" />
           <p>{EMAIL}</p>
