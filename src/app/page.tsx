4fdf19cf8c5b3e20699d3db58244719d64534575
Alfie Qashwa <alfieqashwa@gmail.com>
2024-05-05 16:04:21 +0700

    feat: add home content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,149 @@
+"use client"
+
+import { Clapperboard } from "lucide-react"
 import Image from "next/image"
-import { Button } from "~/components/ui/button"
+import { useMediaQuery } from "./hooks/use-media-query"
 
-const IMAGES = [
-  { src: "/images/23.png", alt: "23" },
-  { src: "/images/24.png", alt: "24" },
-  { src: "/images/25.png", alt: "25" },
-  { src: "/images/26.png", alt: "26" },
-  { src: "/images/27.png", alt: "27" },
-  { src: "/images/28.png", alt: "28" },
-  { src: "/images/29.png", alt: "29" },
-] as const
 export default function Home() {
-  return (
-    <main className="flex min-h-[calc(100vh_-_8rem)] flex-col items-center justify-between pt-12">
-      <h1 className="text-4xl font-bold capitalize lg:text-6xl">home</h1>
+  const isDesktop = useMediaQuery("(min-width: 1024px)")
 
-      {/* <div>
-        {IMAGES.map((img, i) => (
-          <Image
-            src={img.src}
-            alt={img.alt}
-            width={1200}
-            height={1200}
-            key={i}
-            className="py-4"
-          />
+  return (
+    <main className="min-h-[calc(100vh_-_8rem)] py-12">
+      <div className="px-4">
+        {["quick explore", "city snapshot", "local gems"].map((text, i) => (
+          <article key={i} className="flex items-center space-x-2">
+            <Clapperboard />
+            <p className="whitespace-nowrap text-2xl font-semibold capitalize lg:text-3xl">
+              {text}
+            </p>
+          </article>
         ))}
+      </div>
+      <div className="flex flex-col gap-12 py-4 lg:flex-row lg:justify-center lg:gap-8">
+        {isDesktop ? (
+          <div className="flex flex-row justify-center gap-6 py-12">
+            <Image
+              src="/images/22.png"
+              alt="content"
+              width={300}
+              height={300}
+              className="object-cover"
+            />
+            <Image
+              src="/images/24.png"
+              alt="content"
+              width={400}
+              height={400}
+              className="object-cover"
+            />
+            <Image
+              src="/images/22.png"
+              alt="content"
+              width={300}
+              height={300}
+              className="object-cover"
+            />
+          </div>
+        ) : (
+          <div className="flex flex-col items-center gap-12 py-4">
+            <Image
+              src="/images/22.png"
+              alt="content"
+              width={600}
+              height={300}
+              className="object-cover"
+            />
+            <Image
+              src="/images/24.png"
+              alt="content"
+              width={600}
+              height={600}
+              className="object-cover"
+            />
+          </div>
+        )}
+      </div>
+
+      <div className="flex flex-col items-end px-4">
+        {["jakarta highlights", "cultutral snippet", "family fun day"].map(
+          (text, i) => (
+            <article key={i} className="flex items-center space-x-2">
+              <Clapperboard />
+              <p className="whitespace-nowrap text-2xl font-semibold capitalize lg:text-3xl">
+                {text}
+              </p>
+            </article>
+          ),
+        )}
+      </div>
+
+      {isDesktop ? (
+        <div className="flex items-center justify-center gap-6 py-12">
+          <Image
+            src="/images/23.png"
+            alt="content"
+            width={300}
+            height={300}
+            className="object-cover"
+          />
+          <Image
+            src="/images/27.png"
+            alt="content"
+            width={400}
+            height={400}
+            className="object-cover"
+          />
+          <Image
+            src="/images/26.png"
+            alt="content"
+            width={300}
+            height={300}
+            className="object-cover"
+          />
+        </div>
+      ) : (
+        <div className="flex flex-col items-center gap-12 py-4">
+          <Image
+            src="/images/27.png"
+            alt="content"
+            width={600}
+            height={600}
+            className="object-cover"
+          />
+          <Image
+            src="/images/26.png"
+            alt="content"
+            width={600}
+            height={600}
+            className="object-cover"
+          />
+        </div>
+      )}
+      <div className="flex flex-col items-center px-4">
+        {["gulinary delight", "night out"].map((text, i) => (
+          <article key={i} className="flex items-center space-x-2">
+            <Clapperboard />
+            <p className="whitespace-nowrap text-2xl font-semibold capitalize lg:text-3xl">
+              {text}
+            </p>
+          </article>
+        ))}
+      </div>
+      {/* <div className="space-y-12 py-8">
+        <Image
+          src="/images/23.png"
+          alt="content"
+          width={600}
+          height={600}
+          className="object-cover"
+        />
+        <Image
+          src="/images/29.png"
+          alt="content"
+          width={600}
+          height={600}
+          className="object-cover"
+        />
       </div> */}
     </main>
   )
