98ef4e25342f3c7621bda5f15143c0f74fc9758f
Alfie Qashwa <alfieqashwa@gmail.com>
2024-05-05 17:00:31 +0700

    style(bug): fix carousel arrow

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -43,8 +43,8 @@ export function Hero() {
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="hidden md:block" />
-      <CarouselNext className="hidden md:block" />
+      <CarouselPrevious className="hidden md:flex" />
+      <CarouselNext className="hidden md:flex" />
     </Carousel>
   )
 }
